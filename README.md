d14663183674f40c98476cce083a24ef13e1d9d7
kevin <riverosk885@gmail.com>
2020-03-22 21:19:17 -0500

    UPDATE - Readme

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ebookcplusplus
+# Este repositorio contiene todo los codigos usados en el libro Aprende a programar en C++
---
+++ programabasico.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"Mi primer programa en C++";
+    return 0;
+}
+
+// salida: Mi primer programa en C++ 
